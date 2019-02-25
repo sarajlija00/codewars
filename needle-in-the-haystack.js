5bbad0c531af7519ecb2810c03da9262c44c4fac
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-02-25 13:35:35 +0100

    A Needle in the Haystack

---
+++ needle-in-the-haystack.js
@@ -0,0 +1,7 @@
+let arrayOne = [1, 2, 3, 4, 5, 6, 7, 8, 98, 9, 3254, 1243,, 432, 543, "needle", 324, 24,];
+function findNeedle (array){
+  return 'found the needle at position ' + array.indexOf ('needle');
+  }
+  
+  
+  console.log(findNeedle(arrayOne));
